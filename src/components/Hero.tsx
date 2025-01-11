5896bad9331cbdb03489009469681e40a294b8b4
Aniket Bharane <codeaniket123@gmail.com>
2025-01-11 15:35:21 +0530

    hero changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ import { AuroraBackground } from './ui/aurora-background';
 import { TextGenerateEffect } from './ui/text-generate-effect';
 
 function Hero() {
-    const words = ` Hi, I'm Aniket, a college student excelling in React.js. I specialize in crafting web applications with a focus on seamless user experiences. While I've got a solid grasp on React.js, Next.js, Zod, Shadcn and designing web applications using Figma, I'm constantly learning and exploring new concepts to stay ahead in the ever-evolving tech landscape. With effective communication and collaboration skills, I'm ready to bring my passion for web development to your project on Upwork. Let's build something great together!`;
+    const words = ` Hi, I'm Aniket, a college student excelling in React.js and Next.js. I specialize in crafting web applications with a focus on seamless user experiences. While I've got a solid grasp on React.js, Next.js, Zod, Shadcn, Aceternity and designing web applications using Figma, I'm constantly learning and exploring new concepts to stay ahead in the ever-evolving tech landscape. With effective communication and collaboration skills, I'm ready to bring my passion for web development to your project on Upwork. Let's build something great together!`;
 
     return (
         <AuroraBackground>
